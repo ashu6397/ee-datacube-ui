2df7cc1d27708098dac1e3afa850d69501f20f08
ashu6397 <ashutoshvaish6397@gmail.com>
2020-05-10 16:55:26 +0530

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
